c537411e44dce3a8a6784ae4c27e3aa69e14c4f5
Kasper Seweryn <github@wvffle.net>
2021-03-06 11:04:23 +0100

    Add multi file test

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cat read.c > /tmp/a
-./cat read.c > /tmp/b
+cat read.c main.c > /tmp/a
+./cat read.c main.c > /tmp/b
 
 diff /tmp/a /tmp/b && printf "\nTEST SUCCESS\n\n" || printf "\nTEST FAILURE\n\n"
 
